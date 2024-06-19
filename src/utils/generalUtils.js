b457e3d9b5a19c0ae9f08ee9b70867d5a8fb864f
BadaboommS <yann.badaboomms@gmail.com>
2024-06-19 21:28:12 +0200

    changes code decompress

--- src/utils/generalUtils.js
+++ src/utils/generalUtils.js
@@ -11,13 +11,12 @@ export function codeDecompress(code){
         const compressedCode = atob(cleanBase64Code, "base64");
     
         //Inflate code
-        const array = Uint8Array.from([...compressedCode].map(v => v.charCodeAt(0)));
-        var data = pako.inflate(array);
-        var xmlString = String.fromCharCode.apply(null, new Uint16Array(data));
+        const inputArray = Uint8Array.from([...compressedCode].map(v => v.charCodeAt(0)));
+        var data = pako.inflate(inputArray, { to: 'string'});
     
         //Parse items into usable array
         const parser = new DOMParser();
-        const xmlDoc = parser.parseFromString(xmlString, "text/xml");
+        const xmlDoc = parser.parseFromString(data, "text/xml");
         const htmlItems = xmlDoc.getElementsByTagName("Item");
 
         return htmlItems;
